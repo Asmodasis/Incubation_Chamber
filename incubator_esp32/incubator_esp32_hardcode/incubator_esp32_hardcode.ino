a04704b99e5c746818ddf6de4c2ec89e6d8e0fdd
Shawn Ray <Asmodasis@gmail.com>
2024-10-14 17:18:12 -0700

    Temp sensor
    Setup temp sensor for connection and reading

--- incubator_esp32/incubator_esp32_hardcode/incubator_esp32_hardcode.ino
+++ incubator_esp32/incubator_esp32_hardcode/incubator_esp32_hardcode.ino
@@ -1,9 +1,11 @@
 // Designed by Shawn Ray
 // 3/11/2024
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define convertTime(a,b) ( (int)( (double) (a - b) / 3.6*pow(10,6)) )
-const int tempSensorPin = 36;
+const int tempSensorPin = 9;
 const int lightPin = 11;
 const int fanActivatePin = 12;
 const int heatPlateActivatePin = 13;
@@ -14,14 +16,18 @@ unsigned long programStartTime;
 // used for testing
 int testValue = 0;
 
-int dayTempHigh = 28;
+int dayTempHigh = 20;
 int dayTempLow = 18;
-int nightTempHigh = 28;
-int nightTempLow = 18;
+int nightTempHigh = 12;
+int nightTempLow = 8;
 
-int currentTemp = 0;
+float tempCelsius;    // current temperature in Celsius
+float tempFahrenheit; // current temperature in Fahrenheit
+
 int hourLimit = 8;
 
+OneWire oneWire(tempSensorPin);         // setup a oneWire instance 
+DallasTemperature sensors(&oneWire); // pass oneWire to DallasTemperature library
 
 void setup() {
 
@@ -34,15 +40,20 @@ void setup() {
 
   // set pins for output
   // writing to the pins will set the state of the peripheral devices
-  pinMode(lightPin,OUTPUT);
-  pinMode(fanActivatePin,OUTPUT);
-  pinMode(heatPlateActivatePin,OUTPUT);
-  pinMode(tempSensorPin,INPUT);
+  pinMode(lightPin, OUTPUT);
+  pinMode(fanActivatePin, OUTPUT);
+  pinMode(heatPlateActivatePin, OUTPUT);
+  pinMode(tempSensorPin, INPUT);
+  sensors.begin();    // initialize the sensor
 
   // The lights start on (beginning of the day)
   digitalWrite(lightPin, HIGH);
 }
-
+/*void loop(){
+  digitalWrite(lightPin, LOW);
+  digitalWrite(fanActivatePin, LOW);
+  digitalWrite(heatPlateActivatePin, HIGH);
+}*/
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -57,8 +68,19 @@ void loop() {
   seconds %= 60;
   minutes %= 60;
   hours %= 24;
-  currentTemp = analogRead(tempSensorPin);
-  Serial.print(currentTemp);
+  sensors.requestTemperatures();
+  tempCelsius = sensors.getTempCByIndex(0);
+  tempFahrenheit = tempCelsius * 9 / 5 + 32;
+
+  Serial.print("Temperature in Celcius: ");
+  Serial.println(tempCelsius);
+  Serial.print("Temperature in Fahrenheit: ");
+  Serial.println(tempFahrenheit);
+
+  delay(5000);
+  for(int i = 0; i < 16; ++i){
+    Serial.print("\n");
+  }
   delay(1000);
                                 // DayTime
   if(hours < 12){
@@ -66,10 +88,10 @@ void loop() {
     digitalWrite(lightPin, HIGH);
       //  activate heat plates
       //  activate the fan
-    if(currentTemp < dayTempLow){
+    if(tempCelsius < dayTempLow){
       digitalWrite(heatPlateActivatePin, HIGH);
       digitalWrite(fanActivatePin, HIGH);
-    }else if(currentTemp > dayTempHigh){
+    }else if(tempCelsius > dayTempHigh){
       //  deactivate heat plates
       //  activate the fan
       digitalWrite(heatPlateActivatePin, LOW);
@@ -85,10 +107,10 @@ void loop() {
     digitalWrite(lightPin, LOW);
       //  activate heat plates
       //  activate the fan
-    if(currentTemp < nightTempLow){
+    if(tempCelsius < nightTempLow){
       digitalWrite(heatPlateActivatePin, HIGH);
       digitalWrite(fanActivatePin, HIGH);
-    }else if(currentTemp > nightTempHigh){
+    }else if(tempCelsius > nightTempHigh){
       //  deactivate heat plates
       //  activate the fan
       digitalWrite(heatPlateActivatePin, LOW);
